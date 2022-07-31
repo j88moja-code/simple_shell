8a05cd8ede6855b018c7dc30cbc52cc232c325d1
Arnold Mdluli <arnoldmdluli00@gmail.com>
2022-07-31 06:31:02 -0700

    The main file, named it shell.h

---
+++ shell.h
@@ -0,0 +1,49 @@
+#ifndef SHELL_H
+#define SHELL_H
+/**
+ * Libraries used by the project.
+ */
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <stdbool.h>
+
+/* Global constants */
+#define TOKEN_BUFFER_SIZE 64
+#define LINE_SIZE 1024
+
+/**
+ * Environment variables
+ */
+extern char **environ;
+
+/**
+ * The built ins
+ */
+int checker(char **cmd, char *buffer);
+void prompt(void); /*Prompts for input*/
+void *_getline(void); /*Input*/
+char *str_path(char **path, char *args);/*Tests the path command*/
+char *get_path(char **path, char *args);
+int handle_builtin(char **args, char *line);
+void exit_shell(char **arg, char *line); /*Exit the evironment*/
+void env_shell(char **args, char *line); /*Get the environment*/
+
+/**
+ * The stings that will be entered into the environment
+ */
+int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+int _strncmp(char *s1, char *s2, int n);
+char *_strdup(char *s);
+char *_strchr(char *s, char c);
+
+void execution(char *cp, char **cmd);
+char *find_path(void);
+
+#endif /* SHELL_H */
