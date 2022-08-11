91f2e9ba0d4b7f52ebd0fdba62f2ec74101e6807
j88moja-code <104817756+j88moja-code@users.noreply.github.com>
2022-08-11 05:28:00 +0200

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -1,28 +1,131 @@
 #include "shell.h"
 
+#include "shell.h"
+
+#define SETOWD(V) (V = _strdup(_getenv("OLDPWD")))
 /**
- * get_builtin - builtin that pais the command in the arg
- * @cmd: command
- * Return: function pointer of the builtin command
+ * change_dir - changes directory
+ * @data: a pointer to the data structure
+ *
+ * Return: (Success) 0 is returned
+ * ------- (Fail) negative number will returned
  */
-int (*get_builtin(char *cmd))(data_shell *)
+int change_dir(sh_t *data)
 {
-	builtin_t builtin[] = {
-		{ "env", _env },
-		{ "exit", exit_shell },
-		{ "setenv", _setenv },
-		{ "unsetenv", _unsetenv },
-		{ "cd", cd_shell },
-		{ "help", get_help },
-		{ NULL, NULL }
-	};
-	int i;
+	char *home;
 
-	for (i = 0; builtin[i].name; i++)
+	home = _getenv("HOME");
+	if (data->args[1] == NULL)
 	{
-		if (_strcmp(builtin[i].name, cmd) == 0)
-			break;
+		SETOWD(data->oldpwd);
+		if (chdir(home) < 0)
+			return (FAIL);
+		return (SUCCESS);
 	}
+	if (_strcmp(data->args[1], "-") == 0)
+	{
+		if (data->oldpwd == 0)
+		{
+			SETOWD(data->oldpwd);
+			if (chdir(home) < 0)
+				return (FAIL);
+		}
+		else
+		{
+			SETOWD(data->oldpwd);
+			if (chdir(data->oldpwd) < 0)
+				return (FAIL);
+		}
+	}
+	else
+	{
+		SETOWD(data->oldpwd);
+		if (chdir(data->args[1]) < 0)
+			return (FAIL);
+	}
+	return (SUCCESS);
+}
+#undef GETCWD
+/**
+ * abort_hsh - exit the program
+ * @data: a pointer to the data structure
+ *
+ * Return: (Success) 0 is returned
+ * ------- (Fail) negative number will returned
+ */
+int abort_hsh(sh_t *data __attribute__((unused)))
+{
+	int code, i = 0;
 
-	return (builtin[i].f);
+	if (data->args[1] == NULL)
+	{
+		free_data(data);
+		exit(errno);
+	}
+	while (data->args[1][i])
+	{
+		if (_isalpha(data->args[1][i++]) < 0)
+		{
+			data->error_msg = _strdup("Illegal number\n");
+			return (FAIL);
+		}
+	}
+	code = _atoi(data->args[1]);
+	free_data(data);
+	exit(code);
+}
+/**
+ * display_help - display the help menu
+ * @data: a pointer to the data structure
+ *
+ * Return: (Success) 0 is returned
+ * ------- (Fail) negative number will returned
+ */
+int display_help(sh_t *data)
+{
+	int fd, fw, rd = 1;
+	char c;
+
+	fd = open(data->args[1], O_RDONLY);
+	if (fd < 0)
+	{
+		data->error_msg = _strdup("no help topics match\n");
+		return (FAIL);
+	}
+	while (rd > 0)
+	{
+		rd = read(fd, &c, 1);
+		fw = write(STDOUT_FILENO, &c, rd);
+		if (fw < 0)
+		{
+			data->error_msg = _strdup("cannot write: permission denied\n");
+			return (FAIL);
+		}
+	}
+	PRINT("\n");
+	return (SUCCESS);
+}
+/**
+ * handle_builtin - handle and manage the builtins cmd
+ * @data: a pointer to the data structure
+ *
+ * Return: (Success) 0 is returned
+ * ------- (Fail) negative number will returned
+ */
+int handle_builtin(sh_t *data)
+{
+	blt_t blt[] = {
+		{"exit", abort_hsh},
+		{"cd", change_dir},
+		{"help", display_help},
+		{NULL, NULL}};
+	int i = 0;
+
+	while ((blt + i)->cmd)
+	{
+		if (_strcmp(data->args[0], (blt + i)->cmd) == 0)
+			return ((blt + i)->f(data));
+		i++;
+	}
+	return (FAIL);
 }
