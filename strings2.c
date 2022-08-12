973dc3e9ed8cb6fde6b8b3c27cc531f88e84fad3
j88moja-code <sitholent+github@gmail.com>
2022-08-12 06:28:21 -0700

    Update the new func

--- strings2.c
+++ strings2.c
@@ -34,4 +34,58 @@ int _isalpha(int c)
 	}
 	return (FAIL);
 }
+/**
+ * array_rev - reverse array
+ * @arr: the given array
+ * @len: the array length
+ *
+ * Return: void
+ */
+void array_rev(char *arr, int len)
+{
+	int i;
+	char tmp;
 
+	for (i = 0; i < (len / 2); i++)
+	{
+		tmp = arr[i];
+		arr[i] = arr[(len - 1) - i];
+		arr[(len - 1) - i] = tmp;
+	}
+}
+/**
+ * index_cmd - indexed command
+ * @data: a pointer to the data structure
+ *
+ * Return: void
+ */
+void index_cmd(sh_t *data)
+{
+	data->index += 1;
+}
+/**
+ * _strncmp - Compare two strings.
+ * @s1: Pointer to a string.
+ * @s2: Pointer to a string.
+ * @n: The first n bytes of the strings to compare.
+ *
+ * Return: Less than 0 if s1 is shorter than s2.
+ *         0 if s1 and s2 match.
+ *         Greater than 0 if s1 is longer than s2.
+ */
+int _strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+
+	for (i = 0; s1[i] && s2[i] && i < n; i++)
+	{
+		if (s1[i] > s2[i])
+			return (s1[i] - s2[i]);
+		else if (s1[i] < s2[i])
+			return (s1[i] - s2[i]);
+	}
+	if (i == n)
+		return (0);
+	else
+		return (-15);
+}
